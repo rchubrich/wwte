3b33b0419b86b5234ae44e9c9de013cb99c1bf88
Robert Tod Chubrich <chubrich@alumni.stanford.edu>
2013-10-24 12:55:36 -0400

    got map working and sized viewport for mobile

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -6,5 +6,6 @@ function initialize() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);

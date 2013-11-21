6c6704d737d7f259e81de1bb1e9e8ceef64f800c
Robert Tod Chubrich <chubrich@alumni.stanford.edu>
2013-11-21 13:39:06 -0500

    added book button and fixed telephone link

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,3 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessible :description, :latitude, :locu_id, :longitude, :name, :phone, :url, :address
+  attr_accessible :description, :latitude, :locu_id, :longitude, :name, :phone, :url, :address, :opentableurl
 end
